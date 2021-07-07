5c2d525a6ede25ebe71eb66a7b2f429b90cd0270
ᴠᴀꜱᴜ <79586348+xxvasu@users.noreply.github.com>
2021-07-07 10:48:11 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -12,9 +12,7 @@ async def start_(client: Client, message: Message):
     await message.reply_sticker("CAACAgUAAxkBAAIp9mBtwBBZGywWEmV-WC8gcMArjusuAAKMAgACTp1xV6m-mtC1YTfoHgQ")
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name}!
-\nI am ΛLEXA ᴍᴜꜱɪᴄ • @xxvasu ❤
-\nContact us at @Yuki_Support
-\n/help list of available commands.
+\nCongratulations! Now Trixie can play music in the voice chats of Telegram Groups & Channels.
  </b>""",
       
        
@@ -22,24 +20,15 @@ async def start_(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "♬Ɯ៩ន០៣៩ ᖲᖰ០នន០៣ន🎵", url="https://t.me/hindi_chatting_india",
-                    )
-                ],
+                        "➕ ᴀᴅᴅ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕", url=f"https://t.me/TrixieRobot?startgroup=true")],
                 [
                     InlineKeyboardButton(
-                        "👤 Owner", url="https://t.me/xxvasu"
-                    ),
+                        "ᴜᴘᴅᴀᴛᴇꜱ", url=f"https://t.me/vasuxd"), 
                     InlineKeyboardButton(
-                        "💬 Group", url="https://t.me/hindi_chatting_india"
-                    ),
-                    InlineKeyboardButton(
-                        "🔊 Updates", url="https://t.me/vasuxd"
-                    )
-                ],
+                        "ɢʀᴏᴜᴘ", url=f"https://t.me/hindi_chatting_india")],
                 [
                     InlineKeyboardButton(
-                        "➕ Add To Your Group ➕", url="https://t.me/ALEXA_music_robot?startgroup=true"
-                    ) 
+                        "ᴄᴏɴᴛᴀᴄᴛ ᴠᴀꜱᴜ", url=f"https://t.me/xxvasu")
                 ]
             ]
         )
@@ -72,42 +61,3 @@ async def start(client: Client, message: Message):
         )
     )
 
-@Client.on_message(
-    filters.command("help")
-    & filters.private
-    & ~ filters.edited
-)
-async def help(client: Client, message: Message):
-    await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name}!
-\n𝐔𝐒𝐄𝐑 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒
-\n/play <song name> - play song you requested
-/dplay <song name> - play song you requested via deezer
-/splay <song name> - play song you requested via jio saavn
-/playlist - Show now playing list
-/current - Show now playing
-/song <song name> - download songs you want quickly
-/search <query> - search videos on youtube with details
-/deezer <song name> - download songs you want quickly via deezer
-/saavn <song name> - download songs you want quickly via saavn
-/video <song name> - download videos you want quickly
-\n𝐀𝐃𝐌𝐈𝐍𝐒 𝐎𝐍𝐋𝐘
-/player - open music player settings panel
-/pause - pause song play
-/resume - resume song play
-/skip - play next song
-/end - stop music play
-/userbotjoin - invite assistant to your chat
-/admincache - Refresh admin list
-\n𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐰𝐢𝐭𝐡 ❤️ 𝐛𝐲 𝐕𝐀𝐒𝐔
- </b>""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "𝐇𝐄𝐋𝐏 𝐂𝐎𝐍𝐓𝐀𝐂𝐓", url="https://t.me/Yuki_Support"
-                    )
-                ]
-            ]
-        )
-    )    
